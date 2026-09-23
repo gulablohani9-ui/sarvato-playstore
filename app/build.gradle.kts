1e724f8a70fdf935ec7c4af4ca64f483c6ebf0c9
gulablohani9-ui <gulablohani9@gmail.com>
2026-09-23 13:49:17 +0530

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,17 +2,29 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
-    namespace="com.example.sarvatobhadra"
-    compileSdk=35
+    namespace = "com.example.sarvatobhadra"
+    compileSdk = 35
+
     defaultConfig {
-        applicationId="com.example.sarvatobhadra"
-        minSdk=24
-        targetSdk=35
-        versionCode=5
-        versionName="0.5.0"
+        applicationId = "com.example.sarvatobhadra"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 5
+        versionName = "0.5.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
