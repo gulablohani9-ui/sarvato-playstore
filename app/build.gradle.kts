a97178ba751b719a8fae87e9c52be835d5321991
gulablohani9-ui <gulablohani9@gmail.com>
2026-09-23 13:05:25 +0530

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,21 @@
-plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
-android { namespace="com.example.sarvatobhadra"; compileSdk=35
- defaultConfig { applicationId="com.example.sarvatobhadra"; minSdk=24; targetSdk=35; versionCode=2; versionName="0.2.0" }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace="com.example.sarvatobhadra"
+    compileSdk=35
+    defaultConfig {
+        applicationId="com.example.sarvatobhadra"
+        minSdk=24
+        targetSdk=35
+        versionCode=3
+        versionName="0.3.0"
+    }
 }
 dependencies {
- implementation("androidx.core:core-ktx:1.15.0")
- implementation("androidx.appcompat:appcompat:1.7.0")
- implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 }
